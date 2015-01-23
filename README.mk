087b83aab46c6f88e8633bf030e8a52c5e775bc8
Emil Wall <emil.wall@valtech.se>
2015-01-23 16:11:40 +0100

    Initial commit, Yeoman scaffolding setup with Angular generator

---
+++ README.mk
@@ -0,0 +1,22 @@
+FEEDME
+Koncept: Slumpa ett förslag på lunchställe (begränsa radie)
+
+Steg 1: Statisk JSON
+Steg 2: Foursquare API (https://developer.foursquare.com, antagligen bättre anpassat än google places) 
+
+Scaffolding:
+- Lineman med Angular template: https://github.com/linemanjs/lineman-angular-template
+- eller Yeoman med Angular generator: https://github.com/yeoman/generator-angular
+
+Testning:
+- Protractor för att testa Angular-logiken
+- Mocha med Sinon + Chai för modeller
+- Testem.js som testrunner (Karma hade kunnat vara ett alternativ)
+
+
+npm install -g generator-angular
+*** Prata om Lineman och Yeoman, vad Yeoman används till (scaffolding, automatiserad uppsättning)
+
+yo angular
+*** Bootstrap, Sass, Angular-moduler
+
